d7f7783f027cf0fe49311c04416e4ccc631c7d14
LorTho <134836725+LorTho@users.noreply.github.com>
2023-07-26 18:07:28 +0200

    add Test for Exception

--- backend/src/test/java/com/example/backend/service/EmployeeServiceTest.java
+++ backend/src/test/java/com/example/backend/service/EmployeeServiceTest.java
@@ -4,6 +4,10 @@ import com.example.backend.model.Employee;
 import com.example.backend.repository.EmployeeRepo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import static org.mockito.Mockito.*;
 
 class EmployeeServiceTest {
@@ -11,7 +15,7 @@ class EmployeeServiceTest {
     EmployeeService employeeService = new EmployeeService(employeeRepo);
 
     @Test
-    void getEmployeeWithInitialList_whenAddNewEmployee(){
+    void getEmployee_whenAddNewEmployee(){
         //Given
         Employee newEmployee = new Employee("NoId", "test", "test", null, null);
 
@@ -19,5 +23,20 @@ class EmployeeServiceTest {
         Employee actualEmployee = employeeService.addEmployee(newEmployee);
         //Then
         Assertions.assertEquals(newEmployee, actualEmployee);
+    }
+
+    @Test
+    void getEmployee_WhenGetById(){
+        Employee newEmployee = new Employee("1111", "test", "test", null, null);
+
+        when(employeeRepo.findById("1111")).thenReturn(Optional.of(newEmployee));
+        Employee actualEmployee = employeeService.getEmployee("1111");
+
+        verify(employeeRepo).findById("1111");
+        Assertions.assertEquals(newEmployee, actualEmployee);
+    }
+    @Test
+    void getNoSuchElementException_WhenGetByWrongId(){
+        Assertions.assertThrows(NoSuchElementException.class, () -> employeeService.getEmployee("wrongId"));
     }
 }
