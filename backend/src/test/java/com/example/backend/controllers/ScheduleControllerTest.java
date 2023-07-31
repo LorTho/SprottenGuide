1481fcadf4cb96b365a8e2a4a86e901bec2762a8
LorTho <134836725+LorTho@users.noreply.github.com>
2023-07-31 16:02:18 +0200

    unused Import

--- backend/src/test/java/com/example/backend/controllers/ScheduleControllerTest.java
+++ backend/src/test/java/com/example/backend/controllers/ScheduleControllerTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
