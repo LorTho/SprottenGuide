1aac90c33ada6d021c738484dc7c0f4c0969a3e1
LorTho <134836725+LorTho@users.noreply.github.com>
2023-07-31 16:18:14 +0200

    added Test-parameter

--- backend/src/test/java/com/example/backend/service/ScheduleServiceTest.java
+++ backend/src/test/java/com/example/backend/service/ScheduleServiceTest.java
@@ -13,10 +13,11 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 
 class ScheduleServiceTest {
-    ScheduleRepo scheduleRepo= mock(ScheduleRepo.class);
+    ScheduleRepo scheduleRepo = mock(ScheduleRepo.class);
     EmployeeRepo employeeRepo = mock(EmployeeRepo.class);
     EmployeeService employeeService = new EmployeeService(employeeRepo);
     ScheduleService scheduleService = new ScheduleService(scheduleRepo, employeeService);
+
     @Test
     void getWorkSchedule_whenAddNewSchedule() {
         //Given
@@ -29,30 +30,41 @@ class ScheduleServiceTest {
     }
 
     @Test
-    void getSchedule_whenGetScheduleByName() {
+    void getSchedule_whenGetWorkSchedule() {
         //Given
-        WorkScheduleExport exportSchedule = new WorkScheduleExport();
-        exportSchedule.setName("SomeName");
-        exportSchedule.setDrivers(new ArrayList<>());
-        exportSchedule.setKitchen(new ArrayList<>());
-        WorkSchedule workSchedule = new WorkSchedule("SomeId", "SomeName", new ArrayList<>(), new ArrayList<>());
+        WorkScheduleExport expected = new WorkScheduleExport();
+        expected.setName("SomeName");
+        expected.setDrivers(new ArrayList<>(List.of(
+                new ShiftSchedule("MONDAY", List.of(
+                        new WorkShift("Test", 1100))))));
+        expected.setKitchen(new ArrayList<>(List.of(
+                new ShiftSchedule("MONDAY", List.of(
+                        new WorkShift("Test", 1100))))));
+        WorkSchedule workSchedule = new WorkSchedule("SomeId", "SomeName",
+                new ArrayList<>(List.of(
+                        new ShiftSchedule("MONDAY", List.of(
+                                new WorkShift("0000", 1100))))),
+                new ArrayList<>(List.of(
+                        new ShiftSchedule("MONDAY", List.of(
+                                new WorkShift("0000", 1100))))));
         List<WorkSchedule> workScheduleList = new ArrayList<>(List.of(workSchedule));
-        Employee employee = new Employee("0", "Test", "Test", new ArrayList<>(), new ArrayList<>());
+        Employee employee = new Employee("0000", "Test", "Test", new ArrayList<>(), new ArrayList<>());
         //When
         when(scheduleRepo.findAll()).thenReturn(workScheduleList);
-        when(employeeRepo.findById("0")).thenReturn(Optional.of(employee));
+        when(employeeRepo.findById("0000")).thenReturn(Optional.of(employee));
         WorkScheduleExport actualWorkSchedule = scheduleService.getWorkSchedule("SomeName");
         //Then
         verify(scheduleRepo).findAll();
-        Assertions.assertEquals(exportSchedule, actualWorkSchedule);
+        Assertions.assertEquals(expected, actualWorkSchedule);
     }
+
     @Test
-    void getNamesFromID(){
+    void getNamesFromID() {
         Employee employee = new Employee("0000", "Test", "Test", new ArrayList<>(), new ArrayList<>());
         ShiftSchedule shifts = new ShiftSchedule("MONDAY", List.of(
-                        new WorkShift("0000", 1100)));
+                new WorkShift("0000", 1100)));
         ShiftSchedule expected = new ShiftSchedule("MONDAY", List.of(
-                        new WorkShift("Test", 1100)));
+                new WorkShift("Test", 1100)));
 
         when(employeeRepo.findById("0000")).thenReturn(Optional.of(employee));
 
