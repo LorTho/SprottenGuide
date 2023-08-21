a712e0329effd47bb36be92dbcdd8ac95a8a6b7e
LorTho <134836725+LorTho@users.noreply.github.com>
2023-08-21 13:47:44 +0200

    fix tests

--- backend/src/test/java/com/example/backend/service/ScheduleServiceTest.java
+++ backend/src/test/java/com/example/backend/service/ScheduleServiceTest.java
@@ -16,7 +16,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static org.mockito.Mockito.*;
